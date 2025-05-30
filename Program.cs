4547f3e41ece5a2d9839b26fc2d46112d3bd54ca
Danilo Costa <dandan_costa2222@outlook.com>
2025-05-29 23:04:31 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int altura, largura;
+
+Console.WriteLine("--- Retangulo Contorno ---\n");
+
+Console.Write("Largura: ");
+largura = int.Parse(Console.ReadLine()!);
+
+Console.Write("Altura: ");
+altura = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("".PadRight(largura,'*'));
+
+for (int i = 1; i < altura - 1; i++)
+{
+    Console.Write("*".PadRight(largura - 1, ' '));
+    Console.Write("*\n");
+}
+
+Console.WriteLine("".PadRight(largura,'*'));
